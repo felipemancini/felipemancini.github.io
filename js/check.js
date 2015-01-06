feede886feff41ef5f951367d2b239ece9a1d611
Felipe Mancini <fmancini0@gmail.com>
2015-01-06 13:51:15 -0200

    new question

--- js/check.js
+++ js/check.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+    
+    var obj = jQuery.parseJSON( '{"question":[{"meaning":"to forgive someone and be friendly with them again after an argument or disagreement.","answer":"Make up"},{"meaning":"To do something.","answer":"Get up to"},{"meaning":"To relax completely, or not allow things to upset you.","answer":"Chill out"}]}' );
+
+
+    // var myArray = [];
+    // myArray['meaning'] = "to forgive someone and be friendly with them again after an argument or disagreement.";
+    // myArray['answer']  = "Make up";
+
+    // myArray['meaning1'] = "to forgive someone and be friendly with them again after an argument or disagreement.";
+    // myArray['answer1']  = "Make up";
+
+// for (var key in myArray) {
+//     console.log(key);
+// }
+
+    // console.log( myArray ); return false;
 
     $("#check").click(function(){
 
